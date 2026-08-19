ce0a3bef9dc647ff83da63e93cd1282bc671fa27
root <root@ip-172-31-16-82.us-east-2.compute.internal>
2026-08-19 10:14:46 +0000

    Fix gateway CORS for EC2 frontend

--- services/gateway/main.py
+++ services/gateway/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Insurance API Gateway")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "http://18.218.23.226:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
